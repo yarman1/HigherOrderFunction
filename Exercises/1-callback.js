82485a5067c54f235888ced06357d4121d6ff0d2
yarman1 <gordienko045@gmail.com>
2021-12-05 14:01:32 +0200

    lab 8

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,10 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+const iterate = (obj, callback) => {
+  for (const key in obj) {
+    const valueOfKey = obj[key];
+    callback(key, valueOfKey, obj);
+  }
+};
 
 module.exports = { iterate };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,15 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  for (const key in args) {
+    const type = types[key].name.toLowerCase();
+    const argType = typeof args[key];
+    if (argType !== type) throw new Error('TypeError');
+  }
+  const res =  fn(...args);
+  const resType = types[types.length - 1].name.toLowerCase();
+  if (typeof res !== resType) throw new Error('TypeError');
+  return res;
+};
 
 module.exports = { contract };
